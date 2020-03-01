dd810b9f899b6dd2c63c2d228deb1cb3f9cb3e51
SClovesgtx <clovesgtx@gmail.com>
2020-03-01 17:58:06 -0300

    ...

--- Image-Processing-and-Computer-Vision/imutils.py
+++ Image-Processing-and-Computer-Vision/imutils.py
@@ -14,6 +14,25 @@ def generateRadomColor():
     r = random.randint(0, 255)
     return (b, g, r)
 
+def resizeImage(image, width = None, height = None, inter = cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    return resized
+
 def translate(image, x, y):
     M = np.float32([[1, 0, x], [0, 1, y]]) # matriz de transformação
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
